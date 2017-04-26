9a886ccd6d59e0d571b73a0650c74706b8625aba
SauravYadav12 <saurav1286@gmail.com>
2017-04-26 14:27:39 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test{
+	
+	static{
+		print(10);
+	}
+	static void print (int x) {
+		System.out.println (x);
+		System.exit(0);
+	}
+	
+	
+	// The above code will not throw an error at compile time.
+// But at runtime it will throw an error 
+
+public static void main(String[] args)
+	{						  // to make it run properly we need to define a main function.
+							 
+	}
+}
